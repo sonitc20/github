b6c9a83446e50f279ec6a6c0e87a8d01edd63d83
sonitc20 <transon131021@gmail.com>
2021-08-06 23:11:56 +0700

    upcode

---
+++ github/assets/js/index.js
@@ -0,0 +1,30 @@
+var button1 = document.getElementById("btn1");
+    var button2 = document.getElementById("btn2");
+    var button3 = document.getElementById("btn3");
+    var button4 = document.getElementById("btn4");
+    var button5 = document.getElementById("btn5");
+    var button6 = document.getElementById("btn6");
+    var button7 = document.getElementById("btn7");
+    var div = document.getElementById('content');
+
+    button1.onclick = function () {
+                div.style.backgroundImage ="url(/assets/image/tour1.jpg)";
+            };
+    button2.onclick = function () {
+                div.style.backgroundImage ="url(/assets/image/sangolf.jpg)";
+             };  
+    button3.onclick = function () {
+        div.style.backgroundImage ="url(/assets/image/tour2.jpg)";
+    }; 
+    button4.onclick = function () {
+        div.style.backgroundImage ="url(/assets/image/tour3.jpg)";
+    }; 
+    button5.onclick = function () {
+        div.style.backgroundImage ="url(/assets/image/tour4.jpg)";
+    }; 
+    button6.onclick = function () {
+        div.style.backgroundImage ="url(/assets/image/tour5.jpg)";
+    }; 
+    button7.onclick = function () {
+        div.style.backgroundImage ="url(/assets/image/tour6.jpg)";
+    }; 
